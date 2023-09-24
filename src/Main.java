361381eb7b2991b4a2e7007a33785f231fd3ed05
ndmsnkri <144985362+ndmsnkri@users.noreply.github.com>
2023-09-24 18:15:43 +0300

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Create a Scanner object to get user input
+        Scanner in = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Prompt the user to enter their age
+        System.out.print("Enter your age: ");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        // Read the user's age as an integer
+        int age = in.nextInt();
+
+        // Check if the user is 21 or older and display a message
+        if (age >= 21) {
+            System.out.println("Congratulations! You get a wristband.");
+        } else {
+            // If the user is not 21 or older, the program does nothing
+            System.out.println("Sorry, you are not eligible for a wristband.");
+            //ask nad about this
         }
     }
 }
